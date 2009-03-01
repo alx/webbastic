df4fa7aaa402780f6199f9a57a910b0928f8d416
Alexandre Girard <alx.girard@gmail.com>
2009-03-01 11:58:50 +0100

    put admin menu in helper

--- lib/webbastic/helpers.rb
+++ lib/webbastic/helpers.rb
@@ -1,11 +1,12 @@
 helpers = Pathname(__FILE__).dirname.expand_path / "helpers"
 require helpers / "assets"
+require helpers / "content"
 require helpers / "widgets"
 
 module Webbastic
   module Helpers
     def self.setup
-      [Assets, Widgets].each do |helper|
+      [Assets, Content, Widgets].each do |helper|
         ::Merb::GlobalHelpers.send(:include, helper)
       end
     end
