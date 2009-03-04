1ab07766ed1e42367c1a33eb878a4143c5926034
Alexandre Girard <alx.girard@gmail.com>
2009-03-04 05:27:19 +0100

    add media_rocket widget

--- lib/webbastic/helpers/widgets.rb
+++ lib/webbastic/helpers/widgets.rb
@@ -22,6 +22,31 @@ module Webbastic
         end
         
       end
+     
+      class MediaListWidget < Webbastic::Widget
+        def initialize(options)
+          super
+          self.name = "Media List"
+          self.page_id = options[:page_id]
+          super
+        end
+
+        def widget_headers
+          [['gallery_id', 1]]
+        end
+
+        def widget_content
+          gallery_id = self.headers.first(:key => 'gallery_id').value
+          gallery = ::MediaRocket::Gallery.first(:id => gallery_id)
+          medias = gallery.medias.select{|media| media.original?}
+
+          content = "<ul>"
+          medias.each do |media|
+            content << "<li><a href='#{media.url}'>#{media.url}</a></li>"
+          end
+          content << "</ul>"
+        end
+      end
       
     end
   end
