6d15924518a52a153c2fffaf9554f1ac1dcbbd97
Alexandre Girard <alx.girard@gmail.com>
2009-03-11 17:30:51 +0100

    add switch on mime

--- public/javascripts/master.js
+++ public/javascripts/master.js
@@ -41,8 +41,8 @@ $(document).ready(function() {
 			function(data){
 				$.each(data.medias, function(i,media){
 					$("#webbastic_list #thumbs").append(
-						"<a class='media_thumb' rel='" + media.url + "'>" +
-						"<img src='" + media.icon + "' title='" + media.title + "'/>" +
+						"<a class='media_thumb' rel='" + media.url + "' type='" + media.mime + "'>" +
+						"<img src='" + media.icon + "' alt='" + media.title + "'/>" +
 						"</a>"
 					);
 					if ( i == 6 ) $("#webbastic_list #thumbs").append("<br/>");
@@ -60,10 +60,10 @@ $(document).ready(function() {
 	// call method to load media_thumb_list
 	// with first gallery
 	//
-	$("#media_gallery_select").change(function() {
+	$(".media_gallery_select").change(function() {
 		// call method to load media_thumb_list
 		// with selected gallery
-		load_media_thumb_list($("#webbastic_list option:selected").val());
+		load_media_thumb_list(this.value);
 	});
 	
 	//
@@ -71,7 +71,7 @@ $(document).ready(function() {
 	// call method to load media_thumb_list
 	// with first gallery
 	//
-	$("#media_gallery_select").ready(function() {
+	$(".media_gallery_select").ready(function() {
 		load_media_thumb_list(1);
 	});
 	
@@ -86,8 +86,18 @@ $(document).ready(function() {
 			//
 			// User click on a thumbnail to be displayed in editor
 			//
-			$('a.media_thumb').livequery('click', function(event) { 
-				wym.insert("<img src='" + this.rel + "'/>");
+			$('a.media_thumb').livequery('click', function(event) {
+				switch(this.type){
+					case "jpg":
+					case "jpeg":
+					case "gif":
+					case "png":
+						wym.insert("<img src='" + this.rel + "'/>");
+						break
+					default:
+						title = this.children(":first").alt;
+						wym.insert("<a href='" + this.rel + "'/>" + title + "</a>");
+				}
 				return false; 
 			});
         }
