7584b9f93708803fee83ae0224277b018474985c
Alexandre Girard <alx.girard@gmail.com>
2009-04-20 10:41:00 +0200

    Add widget to just show textarea (without wymeditor)

--- lib/webbastic/helpers/widgets.rb
+++ lib/webbastic/helpers/widgets.rb
@@ -32,6 +32,33 @@ module Webbastic
         
       end
       
+      # Store static content in this widget without the need for editor
+      module CodeWidget
+        
+        def edit_partial
+          
+          input_method = self_closing_tag :input, {:type => :hidden, 
+                                                   :name => :_method, 
+                                                   :value => :put}
+                                                   
+          input_area = tag :textarea, "#{self.content}", {:name => "widget[content]", 
+                                                          :rows => 20, 
+                                                          :cols => 100,
+                                                          :class => "contentarea"}
+                                          
+          submit = self_closing_tag :input, {:type => :submit, 
+                                             :value => "Update Content", 
+                                             :class => "wymupdate"}
+          
+          
+          form = tag :form, input_method + input_area + submit, {:action => Merb::Router.url(:webbastic_widget, :id => self.id), 
+                                                                 :method => :post}
+                      
+          form
+        end
+        
+      end
+      
       module RssWidget
         
         def edit_partial
