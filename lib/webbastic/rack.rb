5ea32f2d1b2824b84d8b379634f7d3ed1e673fb9
Alexandre Girard <alx.girard@gmail.com>
2009-04-29 02:30:45 +0200

    add Rack middleware loader

---
+++ lib/webbastic/rack.rb
@@ -0,0 +1,9 @@
+
+module Webbastic
+  module Rack
+    def self.setup
+      Merb.logger.info! "[Rack] loading PageSlug"
+      Merb::Rack.autoload :PageSlug, 'webbastic/rack/page_slug'
+    end
+  end
+end
