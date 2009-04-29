5f299d137d450495c3e664ee9471b731b8c22ea7
Alexandre Girard <alx.girard@gmail.com>
2009-04-29 04:25:45 +0200

    load widgets after slice loaded

--- lib/webbastic.rb
+++ lib/webbastic.rb
@@ -31,6 +31,7 @@ if defined?(Merb::Plugins)
     # Stub classes loaded hook - runs before LoadClasses BootLoader
     # right after a slice's classes have been loaded internally.
     def self.loaded
+      require "webbastic/helpers/widgets"
       ::Webbastic::Helpers.setup
     end
     
