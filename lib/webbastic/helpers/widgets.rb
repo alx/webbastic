deb025da0c31882890075622ab6beac58ac22dce
Alexandre Girard <alx.girard@gmail.com>
2009-05-11 16:47:23 +0200

    Use full size image in MediaList widget

--- lib/webbastic/helpers/widgets.rb
+++ lib/webbastic/helpers/widgets.rb
@@ -260,7 +260,7 @@ module Webbastic
           log "list_html: #{medias.size}"
           medias.each do |media|
             log "media: #{media.title}"
-            list << "<li><img src='" << media.icon << "'><br>" << media.title << "</li>"
+            list << "<li><img src='" << media.url << "'><br>" << media.title << "</li>"
           end
           "<ul>#{list}</ul>"
         end # def list_html
