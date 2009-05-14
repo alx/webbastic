27bba0ffb05b5adb7447a9083261b28a1936fe91
Alexandre Girard <alx.girard@gmail.com>
2009-05-14 03:00:42 +0200

    move js to master.js for quick edit

--- lib/webbastic/widget_modules/gallery_builder.rb
+++ lib/webbastic/widget_modules/gallery_builder.rb
@@ -18,79 +18,10 @@ module Webbastic
         columns_header = self.has_header?(:gallery_columns) || self.add_header(:gallery_columns, 4)
         update_script = "
         $(document).ready(function() {
-          
            function post_header_value(header_name, header_value) {
              var data = '_method=PUT&header[name]=' + header_name + '&header[content]='+header_value;
              $.post('#{Merb::Router.url(:webbastic_widget, :id => self.id)}', data);
            }
-           
-           function post_displayed_galleries() {
-             var widget_content = '';
-             $('input.checkbox_gallery:checked').each(function(index, item){
-              gallery_id = item.name.split('_').pop();
-              widget_content += gallery_id + ',';
-             });
-             post_header_value('displayed_galleries', widget_content);
-           }
-           
-           $('input.checkbox_gallery').click(function() {
-             post_displayed_galleries();
-           });
-       
-           $('a.select_all').click(function() {
-             $('input.checkbox_gallery').attr('checked', true);
-             post_displayed_galleries();
-           });
-       
-           $('a.deselect_all').click(function() {
-             $('input.checkbox_gallery').attr('checked', false);
-             post_header_value('displayed_galleries', 0);
-           });
-           
-           
-           // User has selected mode-display for rel gallery
-           $('input.mode-display').click(function() {
-             var gallery_id = $(this).attr('class', function(className) {
-               return className.match(/gallery-(\d+)/)[1]
-             });
-             var header_value = $('span.edit_header.linked_galleries').value;
-             
-             // Replace gallery link in header by comma, if already present
-             // regexp reading: 1http://abc.com,2http://bcd.com -> [,gallery_id|http...,]
-             var match = new RegExp(','+gallery_id+'.*?,','i').exec(header_value);
-             
-             // Only make a new post if gallery can be deleted
-             if(match != null & match[1].length > 0) {
-               header_value.replace(match[1], ',')
-               post_header_value('linked_galleries', header_value);
-             }
-           });
-           
-            $('input.mode-external').click(function() {
-              
-              // Fetch gallery_id from input.rel attribute
-              var gallery_id = $(this).attr('class', function(className) {
-                 return className.match(/gallery-(\d+)/)[1]
-               });
-              // Fetch current header[linked_galleries] value
-              var header_value = $('span.edit_header.linked_galleries').value;
-              
-              // Uncheck on first pass
-              $('input.mode-display.gallery-'+gallery_id)
-              
-              // Replace gallery link in header by comma, if already present
-              // regexp reading: 1http://abc.com,2http://bcd.com -> [,gallery_id|http...,]
-              var match = new RegExp(','+gallery_id+'.*?,','i').exec(header_value);
-              if(match != null && match[1].length > 0) header_value.replace(match[1], ',')
-              
-              jPrompt('URL Externe:', 'http://', 'Gallery Mode', function(r) {
-                if( r ) {
-                  // send the current header value with the new [id, link] hash
-                  // inside header[linked_galleries] value
-                  post_header_value('linked_galleries', header_value + ',' + gallery_id + r);
-                } 
-              });
-            });
          });
         "
     
--- public/javascripts/master.js
+++ public/javascripts/master.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-	
+
 	// =====
 	//
 	// Form: clear values when user focus on the textfield
 	//
 	// =====
-	
+
 	$.fn.clear_value = function() {
 		return this.focus(function() {
 			if( this.value == this.defaultValue ) {
@@ -17,26 +17,100 @@ $(document).ready(function() {
 			}
 		});
 	};
-	
+
 	$(".clear").clear_value();
-	
+
 	// =====
 	//
 	// File Tree
 	//
 	// ===== 
-	
+
 	// $('#file_tree').fileTree({ root: '', script: 'content' });
-	
+
 	// =====
 	//
 	// Generate spinner
 	//
 	// =====
-	
+
 	$('a#generate').click( function() {
 		$('span#generate-status').addClass('hidden');
 		$('span#generate-spinner').removeClass('hidden');
 	});
- 
+
+	// =====
+	//
+	// GalleryBuilder edit_partial
+	//
+	// =====
+
+	function post_displayed_galleries() {
+		var widget_content = '';
+		$('input.checkbox_gallery:checked').each(function(index, item){
+			gallery_id = item.name.split('_').pop();
+			widget_content += gallery_id + ',';
+		});
+		post_header_value('displayed_galleries', widget_content);
+	}
+
+	$('input.checkbox_gallery').click(function() {
+		post_displayed_galleries();
+	});
+
+	$('a.select_all').click(function() {
+		$('input.checkbox_gallery').attr('checked', true);
+		post_displayed_galleries();
+	});
+
+	$('a.deselect_all').click(function() {
+		$('input.checkbox_gallery').attr('checked', false);
+		post_header_value('displayed_galleries', 0);
+	});
+
+
+	// User has selected mode-display for rel gallery
+	$('input.mode-display').click(function() {
+		var gallery_id = $(this).attr('class', function(className) {
+			return className.match(/gallery-(\d+)/)[1]
+		});
+		var header_value = $('span.edit_header.linked_galleries').value;
+
+		// Replace gallery link in header by comma, if already present
+		// regexp reading: 1http://abc.com,2http://bcd.com -> [,gallery_id|http...,]
+		var match = new RegExp(','+gallery_id+'.*?,','i').exec(header_value);
+
+		// Only make a new post if gallery can be deleted
+		if(match != null & match[1].length > 0) {
+			header_value.replace(match[1], ',')
+			post_header_value('linked_galleries', header_value);
+		}
+	});
+
+	$('input.mode-external').click(function() {
+
+		// Fetch gallery_id from input.rel attribute
+		var gallery_id = $(this).attr('class', function(className) {
+			return className.match(/gallery-(\d+)/)[1]
+		});
+		// Fetch current header[linked_galleries] value
+		var header_value = $('span.edit_header.linked_galleries').value;
+
+		// Uncheck on first pass
+		$('input.mode-display.gallery-'+gallery_id)
+
+		// Replace gallery link in header by comma, if already present
+		// regexp reading: 1http://abc.com,2http://bcd.com -> [,gallery_id|http...,]
+		var match = new RegExp(','+gallery_id+'.*?,','i').exec(header_value);
+		if(match != null && match[1].length > 0) header_value.replace(match[1], ',')
+
+		jPrompt('URL Externe:', 'http://', 'Gallery Mode', function(r) {
+			if( r ) {
+				// send the current header value with the new [id, link] hash
+				// inside header[linked_galleries] value
+				post_header_value('linked_galleries', header_value + ',' + gallery_id + r);
+			} 
+		});
+	});
+
 });
